a163460be72a953ff1e02c55d77a73a2362d64ab
박세훈 <psh3253@naver.com>
2021-09-21 22:57:07 +0900

    toDo 저장 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos"
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
-function paintToDo(newToDo) {
+function paintToDo(newToDoObject) {
     const li = document.createElement("li");
+    li.id = newToDoObject.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;
+    span.innerText = newToDoObject.text;
     const button = document.createElement("button");
     button.innerText = "삭제";
     button.addEventListener("click", deleteToDo);
@@ -23,7 +34,21 @@ function onToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newToDo);
+    const newToDoObject = {
+        id: Date.now(),
+        text: newToDo
+    }
+    toDos.push(newToDoObject);
+    paintToDo(newToDoObject);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", onToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
